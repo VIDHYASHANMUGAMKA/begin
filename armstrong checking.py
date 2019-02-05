35f1b09184eb48eaa87de13b73a42ffaa181c581
VIDHYASHANMUGAMKA <37236109+VIDHYASHANMUGAMKA@users.noreply.github.com>
2019-02-05 12:53:07 +0530

    Update armstrong checking.py

--- armstrong checking.py
+++ armstrong checking.py
@@ -1,11 +1,16 @@
-number = int(input("Enter a number: "))
-sum = 0
-t = number
-while t > 0:
-   digit = t  % 10
-   sum += digit ** 3
-   t //= 10
-if number == sum:
-   print("Yes")
-else:
-   print("No") 
+start = int(input("Enter lower range: "))
+end = int(input("Enter upper range: "))
+ 
+for num in range(start + 1,end):
+   
+   sum = 0
+ 
+   
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+ 
+   if num == sum:
+       print(num) 
